65c216fbd7e6aad4a92db0eeeec44cd07f70b9d3
Lee Hicks <leehicks@dreamfactory.com>
2017-02-07 12:42:04 -0500

    Fix registering in service providers.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -18,7 +18,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     use ServiceDocBuilder;
 
-    public function boot()
+    public function register()
     {
         // Add our scripting service types.
         $this->app->resolving('df.service', function (ServiceManager $df) {
@@ -83,7 +83,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
                         },
                     ]));
         });
+    }
 
+    public function boot()
+    {
         // add migrations
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
