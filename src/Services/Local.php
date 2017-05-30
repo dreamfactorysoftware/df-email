ee84ca54f888c7f14c85578b5b5a3431e3f91d6c
Lee Hicks <leehicks@dreamfactory.com>
2017-05-30 09:44:23 -0400

    Add backward compatibility support for existing SMTP configuration

--- src/Services/Local.php
+++ src/Services/Local.php
@@ -3,6 +3,7 @@
 namespace DreamFactory\Core\Email\Services;
 
 use Swift_SendmailTransport as SendmailTransport;
+use Config;
 
 class Local extends BaseService
 {
@@ -11,7 +12,24 @@ class Local extends BaseService
      */
     protected function setTransport($config)
     {
-        $this->transport = static::getTransport(array_get($config, 'command'));
+        if (!empty($command = array_get($config, 'command'))) {
+            $this->transport = static::getTransport($command);
+        } else {
+            // old usage of mail config and env
+            switch (Config::get('mail.driver')) {
+                case 'sendmail':
+                    $this->transport = static::getTransport(Config::get('mail.sendmail'));
+                    break;
+                case 'smtp':
+                    $host = Config::get('mail.host');
+                    $port = Config::get('mail.port');
+                    $encryption = Config::get('mail.encryption');
+                    $username = Config::get('mail.username');
+                    $password = Config::get('mail.password');
+                    $this->transport = Smtp::getTransport($host, $port, $encryption, $username, $password);
+                    break;
+            }
+        }
     }
 
     /**
