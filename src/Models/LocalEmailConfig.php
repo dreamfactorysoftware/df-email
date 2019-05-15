3ee5ebf3d42d52c91023e4fc3d8001a5e7e61362
OlegL <olegl@softwareplanetgroup.com>
2019-05-15 17:17:16 +0300

    Allow default command env

--- src/Models/LocalEmailConfig.php
+++ src/Models/LocalEmailConfig.php
@@ -20,13 +20,20 @@ class LocalEmailConfig extends BaseServiceConfigNoDbModel
      */
     public static function getSchema()
     {
+
+        if(!empty(env('SENDMAIL_DEFAULT_COMMAND'))) {
+            $defaultCommand = env('SENDMAIL_DEFAULT_COMMAND');
+        } else {
+            $defaultCommand = '/usr/sbin/sendmail -bs';
+        }
+
         return [
             'command' => [
                 'name'        => 'command',
                 'label'       => 'Local Command',
                 'type'        => 'string',
                 'allow_null'  => false,
-                'default'     => '/usr/sbin/sendmail -bs',
+                'default'     => $defaultCommand,
                 'description' => 'Local command to be executed to send mail.',
             ]
         ];
