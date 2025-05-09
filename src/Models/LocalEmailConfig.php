80b1ee21b39eb3041ff79065b32e27950ea9c58e
VitaliyHrabovych <vitaliihr@softwareplanet.group>
2025-05-09 13:43:51 +0300

    Add a command column for local_email_config and override its label and description

--- src/Models/LocalEmailConfig.php
+++ src/Models/LocalEmailConfig.php
@@ -2,14 +2,18 @@
 
 namespace DreamFactory\Core\Email\Models;
 
+use DreamFactory\Core\Email\Components\SupportsEmailParameters;
 use DreamFactory\Core\Models\BaseServiceConfigModel;
 
 class LocalEmailConfig extends BaseServiceConfigModel
 {
+    use SupportsEmailParameters;
+
     protected $table = 'local_email_config';
 
     protected $fillable = [
         'service_id',
+        'command',
         'from_name',
         'from_email',
         'reply_to_name',
@@ -22,4 +26,19 @@ class LocalEmailConfig extends BaseServiceConfigModel
 
     // Add this property to define which fields should be treated as dates
     protected $dates = [];
+
+    /**
+     * @param array $schema
+     */
+    protected static function prepareConfigSchemaField(array &$schema)
+    {
+        parent::prepareConfigSchemaField($schema);
+
+        switch ($schema['name']) {
+            case 'command':
+                $schema['label'] = 'Local Command';
+                $schema['description'] = 'Local command to be executed to send mail.';
+                break;
+        }
+    }
 }
